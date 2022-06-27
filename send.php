3e06054e4d4b916f4d5c1a13d345bfa1ac91a4bf
javib15 <39830541+javib15@users.noreply.github.com>
2022-06-27 15:17:23 -0500

    Add file
    Subir el desarrollo al repositorio

---
+++ send.php
@@ -0,0 +1,22 @@
+<?php
+$name = $_POST['name'];
+$mail = $_POST['email'];
+$message = $_POST['message'];
+
+$header = 'From: ' . $mail. " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$message = "Email send by: " . $name. " \r\n";
+$message .= "Email: " . $mail. " \r\n";
+$message .= "Message: " . $_POST['message'] . " \r\n";
+$message .= "Fue enviado... " . date('d/m/Y', time());
+
+$para = "dcastillero@castlegalabogados.com";
+$asunto = 'Contacto desde web';
+mail($para, $asunto, utf8_decode($message), $header);
+include 'http://castlegalabogados.com';
+
+
+?>
